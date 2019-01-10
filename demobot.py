d8329fb57988cf142b175bd91aa783e5b81d12f4
Rohan Warrier <rwar2495@SHARED.SYDNEY.EDU.AU>
2019-01-10 12:19:05 +1100

    compatible with slack

--- demobot.py
+++ demobot.py
@@ -4,7 +4,6 @@ from flask import Flask, request
 # Create your app (web server)
 app = Flask(__name__)
 
-
 # When people visit the home page '/' use the hello_world function
 @app.route('/')
 def hello_world():
@@ -15,7 +14,7 @@ def ncss():
     return 'Welcome to NCSS'
 
 # You can access demobot’s greet command via /greet
-@app.route('/greet')
+@app.route('/greet', methods=['GET','POST'])
 def greet_person():
     # Get the value of the 'name' query parameter
     # request.values is a dictionary (cool!)
